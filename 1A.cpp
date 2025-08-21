def06d541c0b1a825b1db79b81f17e4236b7f94b
loveyyy1405 <08245760loveyyy@proton.me>
2025-08-21 16:32:06 +0800

    Add files via upload

---
+++ 1A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+int main() {
+    string str ;
+    cin>> str;
+    int num1, num2;
+    char op;  // ÓÃÓÚ´æ´¢ÔËËã·û
+    stringstream ss(str);
+    ss >> num1 >> op >> num2;
+    cout <<num1<<"+"<<num2<<"="<< num1 + num2 << endl;
+    return 0;
+}
